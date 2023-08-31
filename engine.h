51434d46eb5f51d3b3b6d3acc540b003d067b222
ponomarenko vladmimir <v2ponomarenko@gmail.com>
2023-08-31 14:26:06 +0300

    engine.cpp, move.h

--- engine.h
+++ engine.h
@@ -12,11 +12,10 @@ public:
     void set_position_from_fen(std::string);
     void make_move(std::string);
     void make_move(Move move);
+    void unmake_move();
     void print_position();
     Move get_best_move(int depth);
     std::vector<Move> get_all_moves();
-    void make_move(Move move);
-    void unmake_move();
 private:
     Board _board;
     Color _next_move;
@@ -29,6 +28,8 @@ private:
     int32_t _rule_50_halfmoves_counter;
     int32_t _full_moves_counter;
     std::vector<Move> maked_moves;
+    Point white_king_pos;
+    Point black_king_pos;
 
     std::vector<Move> get_moves_for_king(char vertical, int horizontal);
     std::vector<Move> get_moves_for_queen(char vertical, int horizontal);
@@ -36,4 +37,7 @@ private:
     std::vector<Move> get_moves_for_bishop(char vertical, int horizontal);
     std::vector<Move> get_moves_for_knight(char vertical, int horizontal);
     std::vector<Move> get_moves_for_pawn(char vertical, int horizontal, Color color);
+    bool is_king_atacked(Color color);
+    bool is_correct_pos(char vertical, int horizontal);
+    bool is_blank_pos(char vertical, int horizontal);
 };
--- move.h
+++ move.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include "engine.h"
 
 // координаты поля на шахматной доске
 struct Point{
@@ -9,9 +10,11 @@ struct Point{
 };
 
 class Move{
+    friend class Engine;
 public:
     Move();
     Move(std::string user_input);
+    Move(Point start, Point end);
     std::string to_string_present();
 private:
     Point _start;
